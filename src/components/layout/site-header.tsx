a93028d662cc8201ea4fdb28240d601723017c06
Mohammed-MEZO <hypermezo4@gmail.com>
2026-07-27 04:47:58 +0300

    style: reuse header action treatment for account

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -33,7 +33,7 @@ export async function SiteHeader({locale}: {locale: AppLocale}) {
         <div className="header-actions">
           <LocaleSwitcher locale={locale} label={t('language')} />
           <ThemeToggle label={t('theme')} />
-          <Link href={`/${locale}/account`} className="header-account-link">
+          <Link href={`/${locale}/account`} className="header-shop-link">
             {t('account')}
           </Link>
           <Link href={`/${locale}/shop`} className="header-shop-link">
