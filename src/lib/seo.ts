c25d95350f7aaa3824479a777d1133ad09b922c5
Mohammed-MEZO <hypermezo4@gmail.com>
2026-07-27 08:44:54 +0300

    feat: add localized canonical URL helpers

---
+++ src/lib/seo.ts
@@ -0,0 +1,10 @@
+import {locales, type AppLocale} from '@/i18n/routing';
+
+export function localizedPath(locale: AppLocale, pathname = '') {
+  const suffix = pathname.startsWith('/') ? pathname : `/${pathname}`;
+  return `/${locale}${suffix === '/' ? '' : suffix}`;
+}
+
+export function languageAlternates(pathname = '') {
+  return Object.fromEntries(locales.map((locale) => [locale, localizedPath(locale, pathname)]));
+}
